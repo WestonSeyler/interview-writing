332f1f8d5985983bc7e0c082275c95838ad00ccf
wjn <jianing.wu@wowjoy.cn>
2024-01-19 22:07:24 +0800

    feat:全排列

--- src/leecode/买卖股票的最佳时机.js
+++ src/leecode/买卖股票的最佳时机.js
@@ -11,3 +11,4 @@ function maxProfit(prices) {
 // [7,1,5,3,6,4]
 
 
+d
--- src/leecode/全排列.js
+++ src/leecode/全排列.js
@@ -1,25 +1,28 @@
-function permute(nums) {
-    const res = [];
-    const path = [];
-    // 
-    let k = nums.length
-    backtracking(nums, k, [])
-    function backtracking(n, k, used) {
-        if (path.length === k) {
-            res.push(Array.from(path));
-            return
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function (nums) {
+    const used = [];
+    const res = []
+
+    function dfs(path) {
+        if (path.length === nums.length) {
+            res.push(path.slice());
         }
-        for (let i = 0; i < k; i++) {
-            if (used[i]) continue;
-            path.push(n[i])
-            used[i] = true;
-            backtracking(n, k, used)
+        //[1,2,3]
+        for (const num of nums) {
+            if (used[num]) continue
+            path.push(num);
+            used[num] = true;
+            dfs(path);[]
             path.pop()
-            used[i] = false
+            used[num] = false
         }
     }
+    dfs([])
     return res
-}
+};
 
 const res = permute([1, 2, 3])
 console.log(res)
